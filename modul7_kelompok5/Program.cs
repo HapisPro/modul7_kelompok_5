dde82992a2e28009c2a4f572a58385898ff93406
nicohzk <nicohzk05@gmail.com>
2025-04-09 08:21:07 +0700

    menambahkan class GlossaryItem_103022300127

--- modul7_kelompok5/Program.cs
+++ modul7_kelompok5/Program.cs
@@ -9,9 +9,12 @@ namespace tpmodul7_kelompok_5
         {
             DataMahasiswa_103022300127 Hizkia = new DataMahasiswa_103022300127();
             TeamMembers_103022300127 TeamHizkia = new TeamMembers_103022300127();
+            GlossaryItem_103022300127 GlossaryHizkia = new GlossaryItem_103022300127();
             Hizkia.ReadJSON();
             Console.WriteLine("");
             TeamHizkia.ReadJSON();
+            Console.WriteLine();
+            GlossaryHizkia.ReadJSON();
             Console.WriteLine();
         }
     }
---
+++ modul7_kelompok5/models/GlossaryItem_103022300127.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok5.models
+{
+    public class GlossDef {
+        public string para { get; set; }
+        public string GlossSeeAlso { get; set; }
+    }
+    public class GlossEntry {
+        public string ID { get; set; }
+        public string SortAs { get; set; }
+        public string GlossTerm { get; set; }
+        public string Acronym { get; set; }
+        public string Abbrev { get; set; }
+        public GlossDef GlossDef { get; set; }
+        public string GlossSee { get; set; }
+    }
+    public class GlossList {
+        public GlossEntry GlossEntry { get; set; }
+    }
+    public class GlossDiv{
+        public string title { get; set; }
+        public GlossList GlossList;
+    }
+    public class glossary { 
+        public string title { get; set; }
+        public GlossDiv glossDiv;
+    }
+    class GlossaryItem_103022300127
+    {
+        public glossary glos { get; set; }
+        public void ReadJSON() {
+            string filePath = Path.GetFullPath("../../../../datas/jurnal7_3_103022300127.json");
+
+            string jsonString = File.ReadAllText(filePath);
+
+            GlossaryItem_103022300127 glossary = JsonSerializer.Deserialize<GlossaryItem_103022300127>(jsonString);
+            Console.WriteLine($"{glos.title} {glos.glossDiv.title} {glos.glossDiv.GlossList.GlossEntry.ID}" +
+                $" {glos.glossDiv.GlossList.GlossEntry.SortAs} " +
+                $"{glos.glossDiv.GlossList.GlossEntry.GlossTerm}" +
+                $"{glos.glossDiv.GlossList.GlossEntry.Acronym} " +
+                $"{glos.glossDiv.GlossList.GlossEntry.Abbrev} " +
+                $"{glos.glossDiv.GlossList.GlossEntry.GlossDef.para} {glos.glossDiv.GlossList.GlossEntry.GlossDef.GlossSeeAlso}" +
+                $"{glos.glossDiv.GlossList.GlossEntry.GlossSee}");
+        }
+    }
+}
