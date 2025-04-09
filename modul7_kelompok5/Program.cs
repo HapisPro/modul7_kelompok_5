7c1ea1e0434b12a71fc0e0b6c981099af1fadcb2
bintang1855 <bintang.a.p1855@gmail.com>
2025-04-09 08:13:47 +0700

    menambahkan class GlossaryItem_103022300058

--- modul7_kelompok5/Program.cs
+++ modul7_kelompok5/Program.cs
@@ -10,9 +10,12 @@ namespace modul7_kelompok5
         {
             DataMahasiswa_103022300058 bintang = new DataMahasiswa_103022300058();
             TeamMembers_103022300058 teamBintang = new TeamMembers_103022300058();
+            GlossaryItem_103022300058 gloss = new GlossaryItem_103022300058();
             bintang.ReadJSON();
             Console.WriteLine("");
             teamBintang.ReadJSON();
+            Console.WriteLine("");
+            gloss.ReadJSON();
         }
     }
 }
---
+++ modul7_kelompok5/models/GlossaryItem_103022300058.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok5.models
+{
+    public class GlossList
+    { 
+    public string GlossEntry { get; set; }
+    }
+    public class GlossEntry
+    {
+        public string ID { get; set; }
+        public string SortAs { get; set; }
+        public string GlossTerm { get; set; }
+        public string Acronym { get; set; }
+        public string Abbrev { get; set; }
+        public string GlossSee { get; set; }
+    }
+    public class GlossaryItem_103022300058
+    {
+        public GlossEntry glossentry { get; set; }
+        public string para { get; set; }
+        public string[] GlossSeeAlso { get; set; }
+
+        public void ReadJSON()
+        {
+            string filePath = Path.GetFullPath("../../../../datas/jurnal7_3_103022300058.json");
+
+            string jsonString = File.ReadAllText(filePath);
+
+            GlossaryItem_103022300058 glossaryBintang = JsonSerializer.Deserialize<GlossaryItem_103022300058>(jsonString);
+
+            Console.WriteLine("\nGlossary Item:");
+
+            for (int i = 0; i <= 0; i++)
+            {
+                //Console.WriteLine($"<{member.nim}> <{member.firstName} {member.lastName}> ({member.age} {member.gender})");
+                Console.WriteLine(glossaryBintang.GlossSeeAlso[i]);
+            }
+        }
+
+    }
+}
