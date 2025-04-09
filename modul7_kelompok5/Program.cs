b641054ee1d067397d276ed47cc4fad02964f0fc
bintang1855 <bintang.a.p1855@gmail.com>
2025-04-09 07:21:25 +0700

    menambahkan class DataMahasiswa_103022300058

--- modul7_kelompok5/Program.cs
+++ modul7_kelompok5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul7_kelompok5;
+using modul7_kelompok5.models;
+
+namespace modul7_kelompok5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa_103022300058 bintang = new DataMahasiswa_103022300058();
+            bintang.ReadJSON();
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ modul7_kelompok5/models/DataMahasiswa_103022300058.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace modul7_kelompok5.models
+{
+
+
+    public class Address 
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+
+    public class Courses
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+    public class DataMahasiswa_103022300058
+    {
+        public Address address { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public List<Courses> courses { get; set; }
+
+        public void ReadJSON()
+        {
+            string filePath = Path.GetFullPath("../../../../datas/jurnal7_1_103022300058.json");
+
+            string jsonString = File.ReadAllText(filePath);
+
+            DataMahasiswa_103022300058 dataBintang = JsonSerializer.Deserialize<DataMahasiswa_103022300058>(jsonString);
+            Console.WriteLine($"Nama: {dataBintang.firstName} {dataBintang.lastName} \nUmur: {dataBintang.age} \nKelamin: {dataBintang.gender} " +
+                $"\nAlamat: {dataBintang.address.streetAddress}, {dataBintang.address.city}, {dataBintang.address.state}");
+
+            Console.WriteLine("\nMata Kuliah:");
+            int i = 1;
+            foreach (var course in dataBintang.courses)
+            {
+                Console.WriteLine($"MK{i} {course.code} - {course.name}");
+                i++;
+            }
+        }
+    }
+}
