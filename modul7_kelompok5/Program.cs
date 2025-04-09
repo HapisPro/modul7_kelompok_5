748f11ceb0e1accdfa976c6291ab9e544dfcad61
alkausarhapis <alkausarhapis@gmail.com>
2025-04-09 07:21:32 +0700

    menambahkan class TeamMembers_103022300069

--- modul7_kelompok5/Program.cs
+++ modul7_kelompok5/Program.cs
@@ -9,8 +9,10 @@ namespace modul7_kelompok5
         static void Main(string[] args)
         {
             DataMahasiswa_103022300069 hafizh = new DataMahasiswa_103022300069();
+            TeamMembers_103022300069 timHafizh = new TeamMembers_103022300069();
             hafizh.ReadJSON();
             Console.WriteLine("");
+            timHafizh.ReadJSON();
         }
     }
 }
