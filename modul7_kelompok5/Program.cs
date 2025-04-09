53f693c1cfa91ecfaf4d374a63709db1b971b475
nicohzk <nicohzk05@gmail.com>
2025-04-09 07:24:28 +0700

    menambahkan class DataMahasiswa_103022300127

--- modul7_kelompok5/Program.cs
+++ modul7_kelompok5/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul7_kelompok5.models;
+using System;
+
+namespace tpmodul7_kelompok_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa_103022300127 Hizkia = new DataMahasiswa_103022300127();
+            Hizkia.ReadJSON();
+            Console.WriteLine(""); ;
+        }
+    }
+}
---
+++ modul7_kelompok5/models/DataMahasiswa_103022300127.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok5.models
+{
+    public class Address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+    public class Courses
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class DataMahasiswa_103022300127
+    {
+        public Address address { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public List<Courses> courses { get; set; }
+
+        public void ReadJSON()
+        {
+            string filePath = Path.GetFullPath("../../../../datas/jurnal7_1_103022300127.json");
+
+            string jsonString = File.ReadAllText(filePath);
+
+            DataMahasiswa_103022300127 dataHizkia = JsonSerializer.Deserialize<DataMahasiswa_103022300127>(jsonString);
+            Console.WriteLine($"Nama: {dataHizkia.firstName} {dataHizkia.lastName} \nUmur: {dataHizkia.age} \nKelamin: {dataHizkia.gender} " +
+                $"\nAlamat: {dataHizkia.address.streetAddress}, {dataHizkia.address.city}, {dataHizkia.address.state}");
+
+            Console.WriteLine("\nMata Kuliah:");
+            int i = 1;
+            foreach (var course in dataHizkia.courses)
+            {
+                Console.WriteLine($"MK{i} {course.code} - {course.name}");
+                i++;
+            }
+        }
+    }
+}   
