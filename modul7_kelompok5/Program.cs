e94515e42737b1ad4e41815f8d899b89697f026f
F3700 <albertfebrian26@gmail.com>
2025-04-09 08:02:53 +0700

    menambahkan kelas TeamMember_103022300003

--- modul7_kelompok5/Program.cs
+++ modul7_kelompok5/Program.cs
@@ -4,6 +4,10 @@ using modul7_kelompok5;
 using modul7_kelompok5.models;
 
 DataMahasiswa_103022300003 Albert = new DataMahasiswa_103022300003();
+TeamMembers_103022300003 teamAlbert = new TeamMembers_103022300003();
 Albert.ReadJSON();
+Console.WriteLine("");
+teamAlbert.ReadJSON();
+Console.WriteLine("");
 
 
---
+++ modul7_kelompok5/models/TeamMembers_103022300003.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok5.models
+{
+    public class Members
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+    class TeamMembers_103022300003
+    {
+        public List<Members> members { get; set; }
+
+        public void ReadJSON()
+        {
+            string filePath = Path.GetFullPath("../../../../datas/jurnal7_2_103022300003.json");
+            string jsonString = File.ReadAllText(filePath);
+
+            TeamMembers_103022300003 memberList = JsonSerializer.Deserialize<TeamMembers_103022300003>(jsonString);
+
+            Console.WriteLine("Team member list:");
+            int i = 1;
+            foreach (var member in memberList.members)
+            {
+                Console.WriteLine($"{member.nim} {member.firstName + member.lastName}  ({member.age + member.gender})");
+                i++;
+            }
+        }
+    }
+}
