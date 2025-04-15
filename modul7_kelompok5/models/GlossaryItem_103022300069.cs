077db9a9936c12c28beb6f4ab289e31f950f0c05
alkausarhapis <alkausarhapis@gmail.com>
2025-04-15 19:47:31 +0700

    minor fix

--- modul7_kelompok5/models/GlossaryItem_103022300069.cs
+++ modul7_kelompok5/models/GlossaryItem_103022300069.cs
@@ -3,43 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 //using static modul7_kelompok5.models.Glossary;
 
 namespace modul7_kelompok5.models
 {
+    public class GlossaryItem
+    {
+        [JsonPropertyName("glossary")]
+        public Glossary Glossary { get; set; }
+    }
+
     public class Glossary
     {
-        public string title { get; set; }
-        public GLossDiv glossdiv { get; set; }
-        public class GLossDiv
-        {
-            public string title { get; set; }
-            public GlossList glosslist { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
 
-            public class GlossList
-            {
-                public GlossEntry glossentry { get; set; }
-                public class GlossEntry
-                {
-                    public string ID { get; set; }
-                    public string SortAs { get; set; }
-                    public string GlossTerm { get; set; }
-                    public string Acronym { get; set; }
-                    public GlossDef glossdef { get; set; }
-                    public class GlossDef
-                    {
-                        public string para { get; set; }
-                        public string[] GlossSeeAlso { get; set; }
-                    }
-                    public string GlossSee { get; set; }
-                }
-            }
-        }
+        [JsonPropertyName("GlossDiv")]
+        public GlossDiv GlossDiv { get; set; }
     }
+
+    public class GlossDiv
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("GlossList")]
+        public GlossList GlossList { get; set; }
+    }
+
+    public class GlossList
+    {
+        [JsonPropertyName("GlossEntry")]
+        public GlossEntry GlossEntry { get; set; }
+    }
+
+    public class GlossEntry
+    {
+        [JsonPropertyName("ID")]
+        public string ID { get; set; }
+
+        [JsonPropertyName("SortAs")]
+        public string SortAs { get; set; }
+
+        [JsonPropertyName("GlossTerm")]
+        public string GlossTerm { get; set; }
+
+        [JsonPropertyName("Acronym")]
+        public string Acronym { get; set; }
+
+        [JsonPropertyName("Abbrev")]
+        public string Abbrev { get; set; }
+
+        [JsonPropertyName("GlossDef")]
+        public GlossDef GlossDef { get; set; }
+
+        [JsonPropertyName("GlossSee")]
+        public string GlossSee { get; set; }
+    }
+
+    public class GlossDef
+    {
+        [JsonPropertyName("para")]
+        public string Para { get; set; }
+
+        [JsonPropertyName("GlossSeeAlso")]
+        public string[] GlossSeeAlso { get; set; }
+    }
+
     class GlossaryItem_103022300069
     {
         public Glossary glossary { get; set; }
+
         public void ReadJSON()
         {
             string filePath = Path.GetFullPath("../../../../datas/jurnal7_3_103022300069.json");
@@ -48,7 +84,7 @@ namespace modul7_kelompok5.models
 
             GlossaryItem_103022300069 glossaryHafizh = JsonSerializer.Deserialize<GlossaryItem_103022300069>(jsonString);
 
-            Console.WriteLine($"{glossaryHafizh.glossary.title}");
+            Console.WriteLine($"{glossaryHafizh.glossary.GlossDiv.GlossList.GlossEntry.ID}");
             
         }
     }
